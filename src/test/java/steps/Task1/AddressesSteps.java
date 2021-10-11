f14298d923df3ffc11c6bcf0114ed8edf80b072a
piotrbojda1 <kontakt@piotrbojda.pl>
2021-10-11 13:31:06 +0200

    Task_1

--- src/test/java/steps/Task1/AddressesSteps.java
+++ src/test/java/steps/Task1/AddressesSteps.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
